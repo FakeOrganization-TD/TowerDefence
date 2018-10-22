d6e4c53a76214777052fe9af9f977f1eb1a72fbf
Pyjter <piotr_zuber@interia.pl>
2018-10-22 20:40:08 +0200

    Base classes for generator

---
+++ FakeTD/Assets/Procedural Map Generator/Generator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator  {
+
+    public int _width { get; set; }
+    public int _height { get; set; }
+
+    public Generator(int width, int height)
+    {
+        _width = width;
+        _height = height;
+    }
+}
