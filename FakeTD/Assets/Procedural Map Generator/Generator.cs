004a20a0c13066b59aef289a312380d95fc60ea0
Pyjter <piotr_zuber@interia.pl>
2018-10-28 21:09:27 +0100

    Terrain generation with perlin noise ready

--- FakeTD/Assets/Procedural Map Generator/Generator.cs
+++ FakeTD/Assets/Procedural Map Generator/Generator.cs
@@ -2,14 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Generator  {
+public class Generator  : MonoBehaviour{
 
-    public int _width { get; set; }
-    public int _height { get; set; }
 
-    public Generator(int width, int height)
+
+    public GameObject dirtPrefab;
+    public GameObject grassPrefab;
+    PerlinNoise noise;
+
+    int minX = 0;
+    int maxX = 32;
+    int minZ = 0;
+    int maxZ = 64;
+    int minY = 0;
+    int maxY = 20;
+
+    void Start()
     {
-        _width = width;
-        _height = height;
+        noise = new PerlinNoise(Random.Range(1000000, 10000000));
+        Regenerate();
+    }
+    private void Regenerate()
+    {
+
+        float width = dirtPrefab.transform.lossyScale.x;
+        float height = dirtPrefab.transform.lossyScale.y;
+        float depth = dirtPrefab.transform.lossyScale.z;
+
+        for (int i = minX; i < maxX; i++)
+        {//columns (x values)
+            for (int k = minZ; k < maxZ; k++)
+            {
+                int columnHeight = 2 + noise.GetNoise(i - minX, k - minZ, maxY - minY - 2);
+                for (int j = minY; j < minY + columnHeight; j++)
+                {//rows (y values)
+                    GameObject block = (j == minY + columnHeight - 1) ? grassPrefab : dirtPrefab;
+                    Instantiate(block, new Vector3(i * width, j * height, k * depth), Quaternion.identity);
+                }
+            }
+        }
     }
 }
---
+++ FakeTD/Assets/Procedural Map Generator/PerlinNoise.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinNoise {
+    long seed;
+
+    public PerlinNoise(long seed)
+    {
+        this.seed = seed;
+    }
+
+    private int random(long x, int range)
+    {
+        return (int)(((x + seed) ^ 5) % range);
+    }
+
+    private int random(long x, long y, int range)
+    {
+        return (int)(((x + y * 65536 + seed) ^ 5) % range);
+    }
+
+    public int GetNoise(int x, int y, int range)
+    {
+        int chunkSize = 16;
+
+        float noise = 0;
+
+        range /= 2;
+
+        while (chunkSize > 0)
+        {
+            int index_x = x / chunkSize;
+            int index_y = y / chunkSize;
+
+            float t_x = (x % chunkSize) / (chunkSize * 1f);
+            float t_y = (y % chunkSize) / (chunkSize * 1f);
+
+            float r_00 = random(index_x, index_y, range);
+            float r_01 = random(index_x, index_y + 1, range);
+            float r_10 = random(index_x + 1, index_y, range);
+            float r_11 = random(index_x + 1, index_y + 1, range);
+
+            float r_0 = lerp(r_00, r_01, t_y);
+            float r_1 = lerp(r_10, r_11, t_y);
+
+            noise += lerp(r_0, r_1, t_x);
+
+            chunkSize /= 2;
+            range /= 2;
+
+            range = Mathf.Max(1, range);
+        }
+
+        return (int)Mathf.Round(noise);
+    }
+
+    float lerp(float l, float r, float t)
+    {
+        return (1 - t) * l + t * r;
+    }
+
+}
