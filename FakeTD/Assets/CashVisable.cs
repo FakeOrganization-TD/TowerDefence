fa66f77bade7910174ac9d9ca726981a630ebcd4
VoodiMSTR <38701684+VoodiMSTR@users.noreply.github.com>
2018-12-04 18:12:56 +0100

    ELO
    Przyciski

---
+++ FakeTD/Assets/CashVisable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CashVisable : MonoBehaviour {
+
+
+    // Update is called once per frame
+    void Update () {
+        if (MoneyAndScores.money < 15)
+        {
+            var colors = GetComponent<Button>().colors;
+            colors.normalColor = Color.red;
+            GetComponent<Button>().colors = colors;
+        }
+        else
+        {
+            var colors = GetComponent<Button>().colors;
+            colors.normalColor = new Color32(83, 166, 255, 255);
+            GetComponent<Button>().colors = colors;
+        }
+    }
+
+
+ 
+}
